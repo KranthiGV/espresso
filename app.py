3f3f6b3ee249a558053251aaf19db83d01cfa021
Kranthi Kiran GV <kranthi.gv@gmail.com>
2023-10-02 18:21:33 -0400

    Final display tweaks. V1

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ from data_handlers import createDigest
 
 st.title("☕️ Espresso | Daily digest of your favorite topics")
 
+# Get API keys from user
 with st.form(key='api_form'):
    metaphor_api_key = st.text_input(label='Enter metaphor API key:', type='password')
    openai_api_key = st.text_input(label='Enter OpenAI API key:', type='password')
    submit_button = st.form_submit_button(label='Update')
 
+# Get topics from user
 with st.form(key='topics'):
-    topics = st.text_input(label='Enter topics (comma separated):')
-    submit_button = st.form_submit_button(label='Create digest')
+    topics = st.text_input(label='What topics do you want digests for? (comma separated):')
+    submit_button = st.form_submit_button(label='Create digests')
 
 if submit_button:
     topics = topics.split(',')
     topics = [topic.strip() for topic in topics]
 
+    # Collate digests
     digests = []
     with st.status('🪄Searching the magicland...', expanded=True) as status:
         for topic in topics:
-            st.write(f"Summarize {topic}...")
+            st.write(f"Summarizing {topic}...")
             digests.append(createDigest(topic, metaphor_api_key, openai_api_key))
         status.update(label="💫Digest created!", state="complete", expanded=False)
 
-
-
+    # Display digests
     tabs = st.tabs(topics)
-
-    
     for idx, tab in enumerate(tabs):
         with tab:
             for idx, item in enumerate(digests[idx]):
--- data_handlers.py
+++ data_handlers.py
@@ -3,12 +3,18 @@ from metaphor_python import Metaphor
 import openai
 
 def lastWeekDate():
+    """
+    Returns date for a week ago
+    """
     today = datetime.now()
     seven_days_earlier = today - timedelta(days=7)
     formatted_date = seven_days_earlier.strftime('%Y-%m-%d')
     return formatted_date
 
 def fetchRecentURLs(topic, metaphor_api_key):
+    """
+    Fetches recent URLs for a given topic
+    """
     metaphor = Metaphor(metaphor_api_key)
     response = metaphor.search(topic,
                                num_results=2,
@@ -20,6 +26,9 @@ def fetchRecentURLs(topic, metaphor_api_key):
     return response
 
 def fetchRawContentsForIds(ids, metaphor_api_key):
+    """
+    Fetches raw contents for a given list of ids (from metaphor APIs)
+    """
     metaphor = Metaphor(metaphor_api_key)
     response = metaphor.get_contents(ids)
 
@@ -28,12 +37,19 @@ def fetchRawContentsForIds(ids, metaphor_api_key):
     return response
 
 def processRecentURLsForTopic(topic, metaphor_api_key):
+    """
+    Fetches recent URLs for a given topic and returns raw contents
+    """
     response = fetchRecentURLs(topic, metaphor_api_key)
     ids = [item.id for item in response.results]
     contents = fetchRawContentsForIds(ids, metaphor_api_key)
     return contents
 
 def createPromptForSummary(contents):
+    """
+    Creates a prompt based on given extracts to generate a summary
+    """
+    
     base = f"""
          Here's HTML extracts of a few webpages. Summarize all the content into a single piece of clear text. Do not talk about summarizing. Do not refer to first, second, third webpages, etc.
          
@@ -48,17 +64,19 @@ def createPromptForSummary(contents):
     return base
 
 def listSources(contents):
-    base = "### Here are the sources:"
-    for doc in contents:
-        base += f"[{doc.title}]({doc.url})"
-        base += f"""  
-        """
-
-
+    """
+    Creates a list of sources for the digest
+    """
+    base = "### Sources:\n\n"
+    for i, doc in enumerate(contents):
+        base += f"{i+1}. [{doc.title}]({doc.url})\n\n"
     return base
 
 
 def createDigest(topic, metaphor_api_key, openai_api_key):
+    """
+    Creates digest for a given topic
+    """
     openai.api_key = openai_api_key
     response = processRecentURLsForTopic(topic, metaphor_api_key)
     
